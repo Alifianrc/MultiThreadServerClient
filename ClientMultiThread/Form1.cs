a57a78e0469f4cd8787d93edfad84be99f71db1c
Alifianrc <62532983+Alifianrc@users.noreply.github.com>
2021-06-12 20:05:15 +0700

    Fixing lilttle bug

---
+++ ClientMultiThread/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ClientMultiThread
+{
+    public partial class Form1 : Form
+    {
+        TcpClient mySocket = new TcpClient();
+        NetworkStream ServerStream;
+
+        int DataSize = 2028;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            // Alwasy try to connecting if failed to connect
+            while (!mySocket.Connected)
+            {
+                mySocket.Connect(IPAddress.Loopback, 88);
+                ServerStream = mySocket.GetStream();
+            }
+
+            Printf("Client Begin!");
+            // Thread for receiving massage
+            Thread receiveThread = new Thread(RecieveMassage);
+            receiveThread.Start();
+        }
+
+        private void RecieveMassage()
+        {
+            while (mySocket.Connected)
+            {
+                byte[] ReceiveData = new byte[DataSize];
+                string ReceiveMassage = null;
+                try
+                {
+                    // Try to receive stream from server
+                    ServerStream.Read(ReceiveData, 0, DataSize);
+                }
+                catch (Exception e)
+                {
+                    Printf("Error Stream : " + e);
+                }
+                // Get the massage
+                ReceiveMassage = Encoding.ASCII.GetString(ReceiveData);
+                // Print it
+                Printf(ReceiveMassage);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Send Massage
+            byte[] SendData = Encoding.ASCII.GetBytes(textBox2.Text); //Printf("Debug : " + textBox2.Text); //<-Save
+            ServerStream.Write(SendData, 0, SendData.Length);
+            ServerStream.Flush();
+
+            // Reset textbox
+            textBox2.Clear();
+        }
+
+        // Print anything in textbox 1
+        // Little complicated because of Thread
+        delegate void SetTextCallBack(string massage);
+        private void Printf(string massage)
+        {
+            if (this.InvokeRequired)
+            {
+                SetTextCallBack temp = new SetTextCallBack(Printf);
+                this.Invoke(temp, new object[] { massage });
+            }
+            else if (massage != "")
+            {
+                textBox1.Text = textBox1.Text + Environment.NewLine + "-> " + massage;
+            }
+        }
+    }
+}
---
+++ ServerMultiThread/ProcessClient.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ServerMultiThread
+{
+    class ProcessClient
+    {
+        List<TcpClient> ClientSocketList;
+        TcpClient mySocket;
+        string Id;
+
+        string filePath = @"D:\Tugas_Kuliah\Sistem_Oprasi_Jaringan_Komputer\FP_MultiClient\ServerMultiThread\Massage_Data.txt";
+
+        // Act like constructor
+        public void StartClient(TcpClient inClientSocket, string clineNo, List<TcpClient> socketList)
+        {
+            this.mySocket = inClientSocket;
+            this.Id = clineNo;
+            ClientSocketList = socketList;
+            // Start a chatting Thread
+            Thread clientThread = new Thread(SendMassage);
+            clientThread.Start();
+        }
+
+        private void SendMassage()
+        {
+            byte[] byteReceived = new byte[1024];
+            string dataReceived =  null;
+
+            byte[] byteSend;
+            string dataSend;
+
+            int massageCount = 0;
+
+            while (mySocket.Connected)
+            {
+                try
+                {
+                    massageCount++;
+                    // Received data stream
+                    NetworkStream networkStream = mySocket.GetStream();
+                    //networkStream.Read(byteReceived, 0, (int)mySocket.ReceiveBufferSize);
+                    // This size is static, make a big gap between massage
+                    networkStream.Read(byteReceived, 0, 1024);
+
+                    // Processing received data
+                    dataReceived = Encoding.ASCII.GetString(byteReceived);
+                    //dataReceived = dataReceived.Substring(0, dataReceived.IndexOf("$"));
+                    string massage = "From Client-" + Id + " : " + dataReceived;
+                    // Write massage in console
+                    Console.WriteLine(massage);
+
+                    // Save massage every single time
+                    SaveMassage(massage);
+
+                    // Send data to all client
+                    dataSend = "Client-" + Id + " : " + dataReceived + " END";
+                    byteSend = Encoding.ASCII.GetBytes(dataSend);
+                    NetworkStream OtherStream;
+                    foreach (TcpClient socket in ClientSocketList)
+                    {
+                        OtherStream = socket.GetStream();
+                        OtherStream.Write(byteSend, 0, byteSend.Length);
+                        OtherStream.Flush();
+                    }
+
+                    // Reset data
+                    dataReceived = "";
+                    byteReceived = new byte[1024];
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Process Client Error : " + e.ToString());
+                }
+            }
+
+            // If disconnect
+            Console.WriteLine("Client No: " + Id + " Disconnected");
+            ClientSocketList.Remove(mySocket);
+        }
+
+        public void SaveMassage(string newMassage)
+        {
+            // Load old massage from txt
+            List<string> MassageList = new List<string>();
+            try
+            {
+                MassageList = File.ReadAllLines(filePath).ToList();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Save Error : " + e);
+            }
+
+            // Add to list
+            MassageList.Add(newMassage);
+
+            // Save all
+            File.WriteAllLines(filePath, MassageList);
+        }
+    }
+}
